7701ea89e242934c83f0714cefbfc00f161046b3
AkshithBellare <akshithnm@gmail.com>
2018-12-20 15:09:22 +0530

    added few line

--- pointerTest.c
+++ pointerTest.c
@@ -8,4 +8,7 @@ intPointer=&number;
 printf("\nAddress of number is            : %p\n",&number);
 printf("\nAddress of intPointer is        : %p\n",(void*)&intPointer);
 printf("\nAddress stored in intPointer is : %p\n",intPointer);
+printf("\nThe value stored in the variable: %d\n",*intPointer);
+//checking the size of the pointer data type
+printf("\nThe size of intPointer is       : %i bytes\n",(int)sizeof(intPointer));
 }
